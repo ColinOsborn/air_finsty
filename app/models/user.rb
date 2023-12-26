c2addfa0d3ca0606260b3333f6e80e2d6fe32895
Colin Osborn <colin.osborn@gmail.com>
2023-12-26 15:53:06 -0700

    WIP implementing FB omniauth and routes cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable, :confirmable
 
   validates :fullname, presence: true, length: { maximum: 50 }
+
+  def self.from_omniauth(auth)
+    user = User.find_by(email: auth.info.email).first
+    # ensure this will behave how we want vs where(email: auth.info.email)
+    if user
+      return user
+    else
+      where(provider: auth_provider, uid: auth.uid).first_or_create do |user|
+        user.email =    auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.fullname = auth.info.name
+        user.image =    auth.info.image
+        user.uid =      auth.uid
+        user.provider = auth.provider
+
+        user.skip_confirmation!
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  # devise_for :users, controllers: { registrations: 'registrations' }
+
+  devise_for :users,
+    path: '',
+    path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks' }
 
   root 'pages#home'
   get 'pages/home'
